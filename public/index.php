e256ca6dc084ccf5dc04a2fed3beaf0f86c82f7a
James.Nunnerley <james.nunnerley@textlocal.com>
2019-01-21 21:39:56 +0000

    DEBUG

--- public/index.php
+++ public/index.php
@@ -1,31 +1,16 @@
 <?php
-echo "public";
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
+phpinfo();exit;
+require 'vendor/autoload.php';
 
-require __DIR__ . '/../vendor/autoload.php';
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
 
-session_start();
-
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
-
-// Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
-
-// Register middleware
-require __DIR__ . '/../src/middleware.php';
-
-// Register routes
-require __DIR__ . '/../src/routes.php';
-
-// Run app
+    return $response;
+});
 $app->run();
+
+//scoreit.cuf1z38zdshp.eu-west-2.rds.amazonaws.com
+//scoreit_db
+//BHr0Pyftr3aCcbzQqeep

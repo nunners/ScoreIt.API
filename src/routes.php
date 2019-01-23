8a7ac5ea0bb528087198fae2b9b9f4b4b74d2047
James.Nunnerley <james.nunnerley@textlocal.com>
2019-01-23 09:56:41 +0000

    Adding default route

--- src/routes.php
+++ src/routes.php
@@ -9,3 +9,9 @@ $app->get('/signin/:username/:password', function ($username, $password) {
     echo ("Username:".$username."<br />");
     echo ("Password:".$password."<br />");
 });
+
+$app->get('/',
+    function (Request $request, Response $response, array $args) {
+        // Render index view
+        return $this->renderer->render($response, 'index.phtml', $args);
+    });

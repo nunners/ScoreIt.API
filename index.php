93809930c55a84d07f0caae36e8f3006ebc33cac
James.Nunnerley <james.nunnerley@textlocal.com>
2019-01-23 08:43:20 +0000

    Playing

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
+$app->get('/foo', function () {
+    echo "Foo!";
 });
 $app->run();
 
+echo "This is a test";
 //scoreit.cuf1z38zdshp.eu-west-2.rds.amazonaws.com
 //scoreit_db
 //BHr0Pyftr3aCcbzQqeep

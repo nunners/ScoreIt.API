b6e8e67eb080c31e78fda19c2afd2bb9c45a1ddf
James.Nunnerley <james.nunnerley@textlocal.com>
2019-01-30 11:24:21 +0000

    User Authentication

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 require 'vendor/autoload.php';
 
-$c = new \Slim\Container(); //Create Your container
+//$container = new \Slim\Container(); //Create Your container
 
 // Instantiate the app
 $settings = require __DIR__ . '/src/settings.php';
 $app = new \Slim\App($settings);
+
+$database = new Database($app->get('settings')['database']);
 
 // Register routes
 require __DIR__ . '/src/routes.php';

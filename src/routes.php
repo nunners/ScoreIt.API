3b4c8c9d5004e74ee9f6793fa2988c4488b38258
James.Nunnerley <james.nunnerley@textlocal.com>
2019-01-30 11:05:24 +0000

    User Authentication

--- src/routes.php
+++ src/routes.php
@@ -5,18 +5,10 @@ $app->get('/test', function () {
     echo ("Hello test!");
 });
 
-$app->get('/signin/{username}/{password}', function ($request, $response,$args) {
+$app->get('/authenticate/{username}/{password}', function ($request, $response,$args) {
     echo "signin";
     print_r($args);
     print_r($request);
     print_r($response);
 
 });
-$app->get('/api', function () {
-    $newResponse = $oldResponse->withJson(array("status"=>"success","message"=>"Success :)"));
-});
-
-$app->get('/', function () {
-    $newResponse = $oldResponse->withJson(array("status"=>"error","message"=>"no command"));
-});
-
